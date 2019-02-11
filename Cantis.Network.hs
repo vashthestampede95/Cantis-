219ed6cb7b396eacd600c7506073b2d682041890
Aryan Kumar Singh <41736750+vashthestampede95@users.noreply.github.com>
2019-02-11 02:34:28 -0800

    Add files via upload

---
+++ Cantis.Network.hs
@@ -0,0 +1,20 @@
+module Cantis.Network
+    ( ConnectionHandle(..)
+    , runTcpServer
+    , runUdpServer
+    , openConnection
+    , HasSecretKey(..)
+    , HasLogging
+    , TransportType(..)
+    , mkAriviEnv
+    , AriviNetworkException(..)
+    ) where
+
+import           Arivi.Env
+import           Arivi.Network.DatagramServer (runUdpServer)
+import           Arivi.Network.Exception      (AriviNetworkException (..))
+import           Arivi.Network.Instance       (openConnection)
+import           Arivi.Network.StreamServer   (runTcpServer)
+import           Arivi.Network.Types          (ConnectionHandle (..),
+                                               TransportType (..))
+import           Arivi.Utils.Logging          (HasLogging)
