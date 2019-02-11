7e07d67f28e55d49533aa47990549b6a7475158b
Aryan Kumar Singh <41736750+vashthestampede95@users.noreply.github.com>
2019-02-11 00:56:31 -0800

    Add files via upload

---
+++ Cantis.Network.Exception.hs
@@ -0,0 +1,36 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+module Cantis.Network.Exception
+    ( CantisNetworkException(..)
+    , mapIOException
+    ) where
+
+import           Codec.Serialise   (DeserialiseFailure (..))
+import           Control.Exception
+import           Crypto.Error      (CryptoError (..))
+
+data AriviNetworkException
+    = NetworkCryptoException CryptoError
+    | NetworkSignatureVerificationFailedException
+    | NetworkSocketException
+    | NetworkWrongParcelException
+    | NetworkTimeoutException
+    | NetworkDeserialiseException DeserialiseFailure
+    | ReplayAttackException
+    deriving (Eq, Ord, Show)
+
+deriving instance Eq DeserialiseFailure
+
+deriving instance Ord DeserialiseFailure
+
+deriving instance Ord CryptoError
+
+instance Exception AriviNetworkException
+
+mapIOException :: (Exception e) => (SomeException -> e) -> IO a -> IO a
+mapIOException f ioa = do
+    aOrFail <- try ioa
+    case aOrFail of
+        Left e  -> throw (f e)
+        Right r -> return r
